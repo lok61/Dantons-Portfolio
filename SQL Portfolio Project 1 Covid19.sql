53c5173c0fd1014d50c0da938545ccb4b698af72
lok61 <111097386+lok61@users.noreply.github.com>
2022-08-11 17:44:14 -0700

    Update SQL Portfolio Project 1 Covid19.sql

--- SQL Portfolio Project 1 Covid19.sql
+++ SQL Portfolio Project 1 Covid19.sql
@@ -4,6 +4,7 @@ Covid 19 Data Exploration
 
 Skills used: CTE's, Temp Tables, Windows Functions, Aggregate Functions, Joins, Convert Data Types, Insert function
 
+Inspiration taken from Alex Freburg
 */
 
 
